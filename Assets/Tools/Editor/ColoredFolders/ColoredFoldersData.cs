a610cfb6ff90dbbe8b81a6f7ae642e1a2753f154
Efe Emre İpek <ipekefeemre@gmail.com>
2025-06-27 21:28:12 +0300

    added custom inspector for folder data

--- Assets/Tools/Editor/ColoredFolders/ColoredFoldersData.cs
+++ Assets/Tools/Editor/ColoredFolders/ColoredFoldersData.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 [System.Serializable]
@@ -12,4 +13,34 @@ public class ColoredFolderEntry
 public class ColoredFoldersData : ScriptableObject
 {
     public List<ColoredFolderEntry> Data = new List<ColoredFolderEntry>();
+}
+
+[CustomEditor(typeof(ColoredFoldersData))]
+public class ColoredFoldersDataInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        ColoredFoldersData data = (ColoredFoldersData)target;
+
+        GUI.enabled = false;
+        base.OnInspectorGUI();
+        
+        GUI.enabled = data.Data.Count > 0;
+        if(GUILayout.Button("CLEAR") && Event.current.button == 0)
+        {
+            if(EditorUtility.DisplayDialog(
+                "Clear Folder Colors",
+                "Are you sure you want to clear all folder color data? This action cannot be undone.",
+                "Clear",
+                "Cancel"))
+            {
+                data.Data.Clear();
+                EditorUtility.SetDirty(data);
+                AssetDatabase.SaveAssets();
+                ColoredFoldersDrawer.Refresh();
+            }
+        }
+
+        GUI.enabled = true;
+    }
 }
