639decc22bad64cec20f73b59d26f581ad5938a0
Efe Emre İpek <ipekefeemre@gmail.com>
2025-07-15 13:27:11 +0300

    added first iteration of TimeScale Slider

--- Assets/Tools/Editor/TimeScale Slider/TimeScaleSlider.cs
+++ Assets/Tools/Editor/TimeScale Slider/TimeScaleSlider.cs
@@ -1,6 +1,36 @@
+using System;
+using UnityEditor;
 using UnityEngine;
+using UnityToolbarExtender;
 
+[InitializeOnLoad]
 public static class TimeScaleSlider
 {
-    
+    private static float timeScale = 1f;
+    private static readonly float minTimeScale = 0f;
+    private static readonly float maxTimeScale = 2f;
+
+    static TimeScaleSlider()
+    {
+        ToolbarExtender.RightToolbarGUI.Add(OnToolbarGUI);
+    }
+
+    private static void OnToolbarGUI()
+    {
+        GUILayout.Space(10);
+        GUILayout.BeginHorizontal("Box");
+
+        GUILayout.Label("TimeScale");
+        timeScale = GUILayout.HorizontalSlider(timeScale, minTimeScale, maxTimeScale, GUILayout.Width(100f));
+        timeScale = EditorGUILayout.FloatField(timeScale, GUILayout.Width(50f));
+        timeScale = Mathf.Clamp(timeScale, minTimeScale, maxTimeScale);
+
+        GUILayout.EndHorizontal();
+        GUILayout.FlexibleSpace();
+
+        if(Application.isPlaying)
+        {
+            Time.timeScale = timeScale;
+        }
+    }
 }
