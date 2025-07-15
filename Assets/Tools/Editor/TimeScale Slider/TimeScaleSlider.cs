3793f74596f8ffd31a9f6fd3fab9aa3688d01a16
Efe Emre İpek <ipekefeemre@gmail.com>
2025-07-15 13:40:31 +0300

    added slider snapping

--- Assets/Tools/Editor/TimeScale Slider/TimeScaleSlider.cs
+++ Assets/Tools/Editor/TimeScale Slider/TimeScaleSlider.cs
@@ -9,6 +9,7 @@ public static class TimeScaleSlider
     private static float timeScale = 1f;
     private static readonly float minTimeScale = 0f;
     private static readonly float maxTimeScale = 2f;
+    private static readonly float snapInterval = 0.1f;
 
     private const string EDITOR_KEY_TIMESCALE = "TIMESCALE_SLIDER";
 
@@ -33,7 +34,8 @@ public static class TimeScaleSlider
         GUILayout.BeginHorizontal("Box");
 
         GUILayout.Label("TimeScale");
-        timeScale = GUILayout.HorizontalSlider(timeScale, minTimeScale, maxTimeScale, GUILayout.Width(100f));
+        float sliderValue = GUILayout.HorizontalSlider(timeScale, minTimeScale, maxTimeScale, GUILayout.Width(100f));
+        timeScale = Mathf.Round(sliderValue / snapInterval) * snapInterval;
         timeScale = EditorGUILayout.FloatField(timeScale, GUILayout.Width(50f));
         timeScale = Mathf.Clamp(timeScale, minTimeScale, maxTimeScale);
         EditorPrefs.SetFloat(EDITOR_KEY_TIMESCALE, timeScale);
