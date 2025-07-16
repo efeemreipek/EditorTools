9175d482fa95976611a2153977faef4c133a03b9
Efe Emre İpek <ipekefeemre@gmail.com>
2025-07-16 13:32:59 +0300

    created ReadOnly custom attribute

--- Assets/Scripts/AttributeTest.cs
+++ Assets/Scripts/AttributeTest.cs
@@ -5,4 +5,5 @@ public class AttributeTest : MonoBehaviour
     [SerializeField] private bool isAlive = false;
     [SerializeField, ShowIf("isAlive")] private float health = 0f;
     [SerializeField, RangeWithStep(0f, 5f, 0.5f)] private float mana = 0f;
+    [SerializeField, ReadOnly] private Vector2 readOnlyTest;
 }
